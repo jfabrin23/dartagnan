51d647107c3122b8af46891eed83a2dda2c0adc5
Johny Fabrin <johfabrin@gmail.com>
2017-12-21 13:06:22 -0200

    Ajustes mobile header e avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,20 +4,26 @@
       <h1>Logo</h1>
     </div>
 
-    <div class="flex basis2">
-      <div class="container center">
-        <div class="link" @click="$router.push('produto')">Produto</div>
-        <div class="link">Teste 2</div>
-        <div class="link">Teste 3</div>
-        <div class="link">Usuários</div>
-        <div class="link">Teste 4</div>
+    <div class="menu-desk">
+      <div class="basis3">
+        <div class="container center">
+          <div class="link" @click="$router.push('produto')">Produto</div>
+          <div class="link">Teste 2</div>
+          <div class="link">Teste 3</div>
+          <div class="link">Usuários</div>
+          <div class="user container center" @click="inAtivar = true" >
+              <img class="avatar" src="../assets/avatar.png">
+              {{user.name}}
+
+              <div v-bind:class="{'teste':inAtivar}">
+              </div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="basis1 user">
-      <div class="container center">
-        <div>{{user.name}}</div>
-      </div>
+    <div class="menu-mobile">
+      <h1>Teste</h1>
     </div>
   </div>
 </template>
@@ -26,6 +32,11 @@ export default {
   computed: {
     user () {
       return this.$store.state.user
+    }
+  },
+  data () {
+    return {
+      inAtivar: false
     }
   }
 }
@@ -57,11 +68,53 @@ export default {
 
   .header .user {
     margin-right: 1vw;
-    color: var(--corSecundaria)
+    color: var(--corSecundaria);
+    -webkit-transition: width 2s;
+    transition: width 2s;
   }
 
   .header .user:hover {
     color: var(--corTextoHover);
     cursor: pointer;
+  }
+
+  .teste {
+    margin-top:10vh;
+    height: 100px;
+    background-color: black;
+    animation: teste 2s ease;
+  }
+
+  @keyframes teste {
+    0% {
+      height: 0px;
+    }
+    100% {
+      height: 100px;
+    }
+  }
+
+  .header .avatar {
+    height: 5vh;
+    border-radius: 50%;
+    border: 2px solid var(--corSecundaria);
+    margin-right: 0.5vw;
+  }
+
+  @media screen and (max-width:480px){
+    .menu-desk {
+        display:none;
+    }
+    .menu-mobile {
+      display: block;
+    }
+  }
+  @media screen and (min-width:481px){
+    .menu-mobile {
+        display:none;
+    }
+    .menu-desk {
+      display: block;
+    }
   }
 </style>
